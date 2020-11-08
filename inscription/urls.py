55ad6054c7212f2743637f81a5569aab12ea6ce0
mr-Anderson <bigshow.stephane@gmail.com>
2020-11-08 13:45:31 +0100

    suppression de l'enregistrement

--- inscription/urls.py
+++ inscription/urls.py
@@ -6,7 +6,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
 
     url(r'^$', adhesion,name='adhesion'),
-    url(r'^enregistrement$', adhesion_login,name='adhesion_login'),
+
 
 ]
 urlpatterns += staticfiles_urlpatterns()
